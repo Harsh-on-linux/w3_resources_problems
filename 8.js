38faada0d9ec8846d165962616cc24c71f04bc56
Harsh-on-linux <harshy1916@gmail.com>
2025-11-07 18:30:06 +0530

    Random number guess game

---
+++ 8.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log(`Guess a Number between 1 to 10`)
+
+rl.question("Enter your number: ", function (guess) {
+  const num = Math.floor(Math.random() * 10) + 1;
+
+  if (num == guess) {
+    console.log(`You win! ${num} is the correct number`);
+  } else {
+    console.log(`You lose! ${num} is the correct number`);
+  }
+  rl.close();
+});
