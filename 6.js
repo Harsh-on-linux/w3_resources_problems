db5173800756afacb9635b513b5dbc724806d0f3
Harsh-on-linux <harshy1916@gmail.com>
2025-11-07 10:03:55 +0530

    testing alias for commit

--- 6.js
+++ 6.js
@@ -9,7 +9,7 @@ const leapYearCalculator = function (year) {
 }
 
 if (leapYearCalculator(year)) {
-  console.log(`${year} is a leap year`);
+  console.log(`${year} is a leap year!`);
 } else {
-  console.log(`${year} is not a leap year`);
+  console.log(`${year} is not a leap year!`);
 }
