0a98f814a68839e7481c5224452263d4506b83ff
Harsh-on-linux <harshy1916@gmail.com>
2025-11-08 08:51:42 +0530

    Multiply or Divide two numbers using input

---
+++ 010.js
@@ -0,0 +1,28 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log(`Enter two numbers`);
+
+rl.question("Enter first number: ", function (num1) {
+  rl.question("Enter second number: ", function (num2) {
+    console.log(`To Multiply these numbers type 1\nTo Divide these numbers type 2`);
+
+    rl.question("Enter your choice: ", function (choice) {
+      if (choice == 1) {
+        console.log(`${num1} * ${num2} = ${num1 * num2}`);
+      } else if (choice == 2) {
+        console.log(`${num1} / ${num2} = ${num1 / num2}`);
+      } else {
+        console.log("Invalid choice!");
+      }
+
+      rl.close();
+    });
+  });
+});
+
