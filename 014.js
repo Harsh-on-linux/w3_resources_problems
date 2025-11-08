8b5be9df8589f0ff86133d34d55bb5aa129ffd0c
Harsh-on-linux <harshy1916@gmail.com>
2025-11-08 21:03:19 +0530

    Printing file extension

---
+++ 014.js
@@ -0,0 +1,5 @@
+// Assign the string "system.php" to the variable filename
+let filename = "system.js";
+
+// Log the result of extracting the file extension using split and pop to the console
+console.log(filename.split('.').pop());
